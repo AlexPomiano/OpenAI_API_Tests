dbd9ff7c57c80ee0d47a8fe1c1d6156082f1e4fb
John Williams <jrw+3@mit.edu>
2023-05-24 19:07:26 +0000

    key change

---
+++ ImageGen.py
@@ -0,0 +1,16 @@
+import os
+import openai
+
+openai.api_key = OPENAI_API_KEY
+
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Hello Alien where are you from?",
+    temperature=0,
+    max_tokens=64,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    stop=["\"\"\""]
+)
+print(response)
