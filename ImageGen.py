fe3912590a3359f503239aa9e5c74dc7272ee62f
John Williams <jrw+3@mit.edu>
2023-05-24 19:25:27 +0000

    imagegen

--- ImageGen.py
+++ ImageGen.py
@@ -1,16 +1,11 @@
 import os
 import openai
 
-openai.api_key = OPENAI_API_KEY
-
-response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="Hello Alien where are you from?",
-    temperature=0,
-    max_tokens=64,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=["\"\"\""]
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+response = openai.Image.create(
+  prompt="a spaceman dancing tango on the moon",
+  n=1,
+  size="1024x1024"
 )
-print(response)
+image_url = response['data'][0]['url']
+print(image_url)
