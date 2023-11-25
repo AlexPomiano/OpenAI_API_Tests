c65f09ce3de0d4d28524bc8e0fb83cc716b68f8c
John Williams <jrw+3@mit.edu>
2023-11-25 16:43:47 +0000

    .gitignore

--- ImageGen.py
+++ ImageGen.py
@@ -4,7 +4,15 @@ from openai import OpenAI
 client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
 
 print("Welcome to ImageGen")
-response = client.images.generate(prompt=" Hi def image of a couple dancing tango\n\n",
-size="1024x1024")
-image_url = response["data"]
-print(image_url)"
+
+response = client.images.generate(
+  model="dall-e-3",
+  prompt="A high def image of train",
+  size="1024x1024",
+  quality="standard",
+  n=1,
+)
+
+image_url = response.data[0].url
+
+print(image_url)
